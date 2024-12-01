63aa81f8858bb230fe474fbb48d0df60fa66470d
jay <38236622+JayJamieson@users.noreply.github.com>
2024-12-01 21:46:28 +1300

    added db persistance methods

--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require (
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/labstack/gommon v0.4.0
 	github.com/spf13/viper v1.16.0
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d
 )
 
 require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -25,6 +28,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
--- handlers/api.go
+++ handlers/api.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
+
+	"database/sql"
+
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type SecretData struct {
@@ -21,12 +26,82 @@ type createdResponse struct {
 
 type SecretStore struct {
 	store map[string]SecretData
+	db    *sql.DB
 }
 
-func New() *SecretStore {
+func New(url string) (*SecretStore, error) {
+	db, err := sql.Open("libsql", url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// stops STREAM_EXPIRED issues with connections timing out but not released
+	db.SetConnMaxIdleTime(9)
+
 	return &SecretStore{
 		store: make(map[string]SecretData),
+		db:    db,
+	}, nil
+}
+
+func (s *SecretStore) set(data *SecretData) error {
+	tx, err := s.db.Begin()
+
+	if err != nil {
+		return err
 	}
+
+	var name string = GetRandomName(0)
+
+	createSql := "INSERT INTO secret(name, value, ttl, created_at, owner) VALUES(?, ?, ?, ?, ?)"
+	_, err = tx.Exec(createSql, name, data.Secret, 0, data.CreatedAt, data.Owner)
+
+	if err != nil {
+		txErr := tx.Rollback()
+		return errors.Join(err, txErr)
+	}
+
+	tx.Commit()
+
+	return nil
+}
+
+func (s *SecretStore) get(name string) (*SecretData, error) {
+	fetchSql := "SELECT value, created_at, owner FROM secret WHERE name = ?"
+	row := s.db.QueryRow(fetchSql, name)
+
+	if err := row.Err(); err != nil {
+		return nil, err
+	}
+
+	var secret SecretData
+	err := row.Scan(&secret.Secret, &secret.CreatedAt, &secret.Owner)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &secret, nil
+}
+
+func (s *SecretStore) remove(name string) error {
+	tx, err := s.db.Begin()
+
+	if err != nil {
+		return err
+	}
+
+	removeSql := "DELETE FROM secret WHERE name = ?"
+
+	_, err = tx.Exec(removeSql, name)
+
+	if err != nil {
+		txErr := tx.Rollback()
+		return errors.Join(err, txErr)
+	}
+
+	return tx.Commit()
 }
 
 func (s *SecretStore) Health(c echo.Context) error {

042934824d3f78a693f6da9be1cde661eb6eb7b9
jay <38236622+JayJamieson@users.noreply.github.com>
2024-12-01 17:33:12 +1300

    fix undefined result variable

--- ui/clipboard.js
+++ ui/clipboard.js
@@ -1,52 +1,57 @@
-let onCopy = () => {}
+let onCopy = () => {};
 
 const copy = async (target, mimeType = undefined) => {
-    if (typeof target === 'function') {
-        target = target()
-    }
+  if (typeof target === "function") {
+    target = target();
+  }
 
-    if (typeof target === 'object') {
-        target = JSON.stringify(target)
-    }
+  if (typeof target === "object") {
+    target = JSON.stringify(target);
+  }
 
-    if (mimeType !== undefined) {
-        const result = await window.navigator.clipboard.write([
-        new ClipboardItem({
-          [mimeType]: new Blob([target], {
-            type: mimeType,
-          })
-        })
-      ])
-      return onCopy(result)
-    }
+  if (mimeType !== undefined) {
+    const result = await window.navigator.clipboard.write([
+      new ClipboardItem({
+        [mimeType]: new Blob([target], {
+          type: mimeType,
+        }),
+      }),
+    ]);
+    return onCopy(result);
+  }
 
-    result = await window.navigator.clipboard.writeText(target)
-  return onCopy(result)
-}
+  let result = await window.navigator.clipboard.writeText(target);
+  return onCopy(result);
+};
 
 function Clipboard(Alpine) {
-    Alpine.magic('clipboard', () => {
-        return copy
-    })
+  Alpine.magic("clipboard", () => {
+    return copy;
+  });
 
-    Alpine.directive('clipboard', (el, { modifiers, expression }, { evaluateLater, cleanup }) => {
-        const getCopyContent = modifiers.includes('raw') ? c => c(expression) : evaluateLater(expression)
-        const clickHandler = () => getCopyContent(copy)
+  Alpine.directive(
+    "clipboard",
+    (el, { modifiers, expression }, { evaluateLater, cleanup }) => {
+      const getCopyContent = modifiers.includes("raw")
+        ? (c) => c(expression)
+        : evaluateLater(expression);
+      const clickHandler = () => getCopyContent(copy);
 
-        el.addEventListener('click', clickHandler)
+      el.addEventListener("click", clickHandler);
 
-        cleanup(() => {
-            el.removeEventListener('click', clickHandler)
-        })
-    })
+      cleanup(() => {
+        el.removeEventListener("click", clickHandler);
+      });
+    }
+  );
 }
 
 Clipboard.configure = (config) => {
-    if (config.hasOwnProperty('onCopy') && typeof config.onCopy === 'function') {
-        onCopy = config.onCopy
-    }
+  if (config.hasOwnProperty("onCopy") && typeof config.onCopy === "function") {
+    onCopy = config.onCopy;
+  }
 
-    return Clipboard
-}
+  return Clipboard;
+};
 
 export default Clipboard;

2e8b19c6bdc9f49a38b59a172dc2e0fe3dea49ee
jay <38236622+JayJamieson@users.noreply.github.com>
2024-12-02 23:42:05 +1300

    added client side encryption

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
-CREATE TABLE secret (
+-- 1/12/2024
+CREATE TABLE IF NOT EXISTS secret (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT UNIQUE,
   value TEXT,
@@ -7,3 +8,13 @@ CREATE TABLE secret (
 );
 
 CREATE INDEX IF NOT EXISTS name_idx ON secret (name);
+
+-- 2/12/2024
+-- add encryption related columns, only ever store ciphered value, iv and salt
+-- even non passphrase values are encrypted
+ALTER TABLE secret ADD iv TEXT;
+ALTER TABLE secret ADD salt TEXT;
+ALTER TABLE secret ADD has_password NUMBER;
+
+-- removed unused column
+ALTER TABLE secret DROP owner;
--- ui/index.js
+++ ui/index.js
@@ -37,3 +37,75 @@ document.addEventListener("DOMContentLoaded", function () {
     window.history.replaceState(null, null, window.location.href);
   }
 });
+
+/**
+ *
+ * @param {string} passphrase
+ * @param {string} salt
+ */
+async function encodePassphrase(passphrase, salt) {
+  const encodedPassphrase = new TextEncoder().encode(passphrase);
+
+  const initialKey = await crypto.subtle.importKey(
+    "raw",
+    encodedPassphrase,
+    { name: "PBKDF2" },
+    false,
+    ["deriveKey"]
+  );
+
+  return await crypto.subtle.deriveKey(
+    { name: "PBKDF2", salt, iterations: 100000, hash: "SHA-256" },
+    initialKey,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["encrypt", "decrypt"]
+  );
+}
+
+async function encrypt(content, passphrase) {
+  const salt = crypto.getRandomValues(new Uint8Array(16));
+
+  const key = await encodePassphrase(passphrase, salt);
+
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+
+  const contentBytes = new TextEncoder().encode(content);
+
+  const cipher = new Uint8Array(
+    await crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, contentBytes)
+  );
+
+  return {
+    salt: bytesToBase64(salt),
+    iv: bytesToBase64(iv),
+    cipher: bytesToBase64(cipher),
+  };
+}
+
+async function decrypt(encryptedData, password) {
+  const salt = base64ToBytes(encryptedData.salt);
+
+  const key = await encodePassphrase(password, salt);
+
+  const iv = base64ToBytes(encryptedData.iv);
+
+  const cipher = base64ToBytes(encryptedData.cipher);
+
+  const contentBytes = new Uint8Array(
+    await crypto.subtle.decrypt({ name: "AES-GCM", iv }, key, cipher)
+  );
+
+  return new TextDecoder().decode(contentBytes);
+}
+
+function bytesToBase64(arr) {
+  return btoa(Array.from(arr, (b) => String.fromCharCode(b)).join(""));
+}
+
+function base64ToBytes(base64) {
+  return Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+}
+
+window.encrypt = encrypt
+window.decrypt = decrypt
